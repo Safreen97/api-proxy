8ae60820f63779aa6c06dfa27945046bb4d22b8d
Safreen97 <92501786+Safreen97@users.noreply.github.com>
2025-03-04 10:41:10 +0800

    Update fetch_employee_API.php

--- fetch_employee_API.php
+++ fetch_employee_API.php
@@ -2,6 +2,7 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Content-Type: application/json");
 
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
